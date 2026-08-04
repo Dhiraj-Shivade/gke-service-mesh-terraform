412aa7c659e4dce5c8952bbd3d1a1c56bf4c3aca
Dhiraj-Shivade <154808307+Dhiraj-Shivade@users.noreply.github.com>
2026-08-04 10:36:08 +0530

    Add time provider to Terraform configuration
    Added time provider with version constraint.

--- environments/prod/versions.tf
+++ environments/prod/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = ">= 1.8.0"
 
   required_providers {
+
     google = {
       source  = "hashicorp/google"
       version = "~> 6.0"
@@ -11,5 +12,11 @@ terraform {
       source  = "hashicorp/google-beta"
       version = "~> 6.0"
     }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12"
+    }
+
   }
 }
